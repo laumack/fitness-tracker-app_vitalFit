a90d600cc8ed17c488501130bd7786c78bc9a64c
laumack <lauren.a.mcintyre@gmail.com>
2023-06-14 10:06:02 +0100

    add files to automate API calls
    and add dataset for 1200 cals, no diet, no intolerances

---
+++ database-seed/meals/2-get_recipe_titles.js
@@ -0,0 +1,45 @@
+/*
+SECOND STEP: Extract the meal titles from the meal plan data.
+Change data at the following places:
+  *1    Replace with the path to the mealPlan file created in step one
+  **2   Amend the file name to ensure that previous data is not overwritten
+When you are sure that ALL parts have been changed, then run the file with "node 2-get_recipe_titles.js"
+*/
+
+const fs = require("fs");
+
+function readJSONFile(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  return JSON.parse(fileData);
+}
+
+function extractTitles(mealPlan) {
+  const allTitles = [];
+
+  Object.values(mealPlan).forEach((day) => {
+    const titles = day.meals.map((meal) => meal.title);
+    allTitles.push(...titles);
+  });
+
+  return allTitles;
+}
+
+function processJSONFile() {
+  const filePath = "mealPlan_week_1200cals.json"; // *1
+
+  const mealPlan = readJSONFile(filePath).week;
+  const titles = extractTitles(mealPlan);
+
+  console.log("Recipe titles:", titles);
+
+  const titlesFileContent = JSON.stringify(titles);
+  fs.writeFile("recipeTitles_week_1200cals.json", titlesFileContent, (err) => { // **2
+    if (err) {
+      console.error("Error writing file:", err);
+    } else {
+      console.log("Recipe titles saved to recipeTitles.json");
+    }
+  });
+}
+
+processJSONFile();
---
+++ database-seed/meals/callsToMake.md
@@ -0,0 +1,29 @@
+Plan A
+
+No diet:
+1200✅    1600✅    2000✅    2500
+
+Vegan:
+1200    1600    2000    2500
+
+All intolerances:
+1200    1600    2000    2500
+
+Vegan & all intolerances:
+1200    1600    2000    2500
+
+16 in total
+
+
+Plan B
+
+No diet:
+1300 1400 1500 1700 1800 1900 2100 2200 2300 2400
+
+Vegetarian (all calories):
+1200 1300 1400 1500 1600 1700 1800 1900 2000 2100 2200 2300 2400 2500
+
+Vegan (remaining calories):
+1300 1400 1500 1700 1800 1900 2100 2200 2300 2400
+
+Split intolerances into individual / smaller groups
