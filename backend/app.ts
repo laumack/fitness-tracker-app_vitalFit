1afb6a178eb8c6a6cc31889256da042a7bdd6d44
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-06-15 11:21:59 +0100

    tests

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,7 @@
-import express, { Express } from 'express';
-import apiRouter from './routes/api.router';
-import cors from 'cors';
-import { customErr, errLog } from './error-handler';
+import express, { Express } from "express";
+import apiRouter from "./routes/api.router";
+import cors from "cors";
+import { customErr, errLog } from "./error-handler";
 
 const app: Express = express();
 app.use(express.json());
